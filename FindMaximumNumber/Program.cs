8570f6a5006c4095eb200f0e6926127527a5ce31
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-09 19:03:37 +0530

    [Shivani] Find Maximum number

---
+++ FindMaximumNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a option to find a maximum number : ");
+            Console.WriteLine(" 1. Find 3 integer maximum number");
+            int option=Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    //IntegerMaximumNumber integerMaximumNumber = new IntegerMaximumNumber();
+                   //nt value = integerMaximumNumber.PrintMaximumNumber(40, 50, 30);
+                    //Console.WriteLine("Maximum Number is " + value);
+                    break;
+                default:
+                    Console.WriteLine("Chosse a right number");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
